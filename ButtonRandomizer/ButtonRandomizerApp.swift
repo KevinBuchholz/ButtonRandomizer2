492dec8c03c1ba72fad40d6739c5ddebcfc9e47b
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-10-02 09:37:50 -0400

    Initial Commit

---
+++ ButtonRandomizer/ButtonRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonRandomizerApp.swift
+//  ButtonRandomizer
+//
+//  Created by Kevin Buchholz on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
